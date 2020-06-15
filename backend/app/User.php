b07e5c6b833c116ed6049c0779fcd8b58c1342b5
Leonardo Persan <leopso1990@gmail.com>
2020-06-14 23:46:35 -0300

    feat: create user, login and logout

--- backend/app/User.php
+++ backend/app/User.php
@@ -2,31 +2,33 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+    protected $hidden = ['password'];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,11 +4,16 @@ use App\Portfolio;
 use App\Jobs\MatchLeads;
 use Illuminate\Http\Request;
 use App\Imports\ClientsImport;
+use App\User;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Queue;
 
-$router->post('/api/portifolios', function (Request $request) use ($router) {
+$router->post('/portifolios', function (Request $request) use ($router) {
+    $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required|email|unique:users'
+    ]);
     $file = $request->file('file');
     $file->store('portifolios/' . date('Y/m/d'));
 
@@ -22,4 +27,40 @@ $router->post('/api/portifolios', function (Request $request) use ($router) {
 
     Queue::push(new MatchLeads($portfolio));
     return response(['result' => true]);
+});
+
+$router->post('/users', function (Request $request) use ($router) {
+    $this->validate($request, [
+        'email' => 'required|email|unique:users',
+        'password' => 'required|confirmed',
+    ]);
+    return User::create([
+        'name' => $request->input('email'),
+        'email' => $request->input('email'),
+        'password' => $request->input('password'),
+    ]);
+});
+
+$router->post('login', function () {
+    $credentials = request(['email', 'password']);
+
+    if (!$token = auth('api')->attempt($credentials)) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'bearer',
+        'expires_in' => auth('api')->factory()->getTTL() * 60
+    ]);
+});
+
+$router->group(['middleware' => 'auth:api'], function ($router) {
+    $router->post('logout', function () {
+        auth('api')->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    });
+    $router->get('me', function () {
+        return response()->json(auth('api')->user());
+    });
 });
